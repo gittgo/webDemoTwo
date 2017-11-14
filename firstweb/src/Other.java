53e9a0d49e109acc9da08384eb572905ad54d347
wangchengwei <wang1100@vip.qq.com>
2017-11-14 21:26:54 +0800

    测试缩写

--- firstweb/src/Other.java
+++ firstweb/src/Other.java
@@ -4,5 +4,9 @@ public class Other extends Some {
     Other(){
         super(18);
         System.out.print("调用Other");
+        System.out.print("调用Other");
+        System.out.print("调用Other");
+
+
     }
 }
